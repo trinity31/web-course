b94464d98ff1c17a40b73e248627adad9cc30c6e
Trinity Song <kyeongyeon.song@gmail.com>
2016-08-11 23:23:12 +0900

    init repo

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var PORT = 8080;
+
+var middleware = {
+	//route level middleware
+	requireAuthentication: function(req, res, next) { 
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next) {
+		console.log('[' + new Date().toString() + '] Request: ' + req.method + '' + req.originalUrl);
+		next();
+	}
+};
+
+app.use(express.static(__dirname + '/public'))
+// console.log(__dirname);
+
+app.listen(PORT, function() {
+	console.log('Express server started, port = ' + PORT);
+});
